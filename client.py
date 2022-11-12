fb88ca61a4e4600056e303a648bf97446b9270ee
DyllasDek <danchik.korneenko@yandex.ru>
2022-11-12 21:53:46 +0300

    Client without functionality, empty server,proto

---
+++ client.py
@@ -0,0 +1,69 @@
+import sys
+import grpc
+import chord_pb2_grpc as pb2_grpc
+import chord_pb2 as pb2
+
+channel = None
+stub = None
+
+
+def send_messages(val):
+    for n in list(map(int, val)):
+        req = pb2.PrimeMessage(
+            num=n)
+        yield req
+
+
+def connect(ip):
+    global channel
+    global stub
+    try:
+        channel = grpc.insecure_channel(ip)
+        stub = pb2_grpc.SimpleServiceStub(channel)
+    except grpc.RpcError as rpc_error:
+        if rpc_error.code() == grpc.StatusCode.UNAVAILABLE:
+            print(f'The server {ip} is unavailable')
+        else:
+            print("Something wrong with grpc, try again.")
+        pass
+
+
+def getleader():
+    #resp = stub.GetLeader(pb2.GetInfo())
+    #print(f'{resp.id} {resp.ip}')
+    print('lox1')
+
+
+def sus(period):
+    #resp = stub.GetChord(pb2.GetInfo())
+
+    # for elem in resp.table:
+    #    print(elem)
+    print('lox2')
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            line = input("> ")
+            if len(line) != 0:
+                line = line.split(' ', 1)
+                if line[0] == "connect":
+                    connect(line[1])
+                    continue
+
+                if line[0] == "getleader":
+                    getleader()
+                    continue
+                if line[0] == 'suspend':
+                    time = line[1].split(' ', 1)
+                    sus(time)
+                if line[0] == 'exit':
+                    break
+        except KeyboardInterrupt:
+            break
+        except:
+            print("Something wrong, try again!")
+            pass
+
+print("Shutting down")
